753852875ce1b7b09e56efad450b206207b89184
Hector Solano <solanoh@me.com>
2017-02-26 16:22:05 -0800

    First commit

---
+++ src/docData.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+
+/**
+ * @author Hector
+ *
+ */
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class docData {
+	int docId;
+	ConcurrentHashMap<String, String> metaData;
+	ConcurrentHashMap<String, String> zones;
+	ConcurrentHashMap<String, String> authors;
+
+	/**
+	 * 
+	 */
+	public docData(int id,ConcurrentHashMap<String, String> data, ConcurrentHashMap<String, String> zone,
+	ConcurrentHashMap<String, String> author){
+		docId = id;
+		metaData = data;
+		zones = zone;
+		authors = author;
+	}
+		
+	// Returns true if the parameter name is one of the documents authors.
+	public boolean termIdisAuthor(String  author){
+		return authors.getOrDefault(author, null) == author;
+	}
+	
+	// needs to be implemented
+	public String termIdisTitle(){
+		return zones.getOrDefault("title", null);
+	}
+
+	
+	// needs to be implemented
+	public boolean termIdinAbstract(String termId){
+		return zones.get("abstract") == termId;
+	}
+	
+	// returns date of document creation
+	public String dateOfCreation(String date){
+		return metaData.getOrDefault("dateOfCreation", null);
+	}
+	
+	// return document type
+	public String docType(){
+		return metaData.getOrDefault("docType", null);
+	}
+	
+	
+		/*
+		metaData = new ConcurrentHashMap<String, String>();
+		metaData.put("authors", "None");
+		metaData.put("dateOfCreation", "None");
+		metaData.put("title", "None");
+		metaData.put("docType", "None");
+	
+		zones.put("author", "None");
+		zones.put("title", "None");
+		zones.put("abstract", "None");	
+		*/	
+}
---
+++ src/post.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+/**
+ * @author Hector
+ *
+ */
+
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class post {
+	int docId;
+	int termIdCount;
+	String termId;
+	ConcurrentHashMap<String, String> metaData;
+	ConcurrentHashMap<String, String> zones;
+	
+
+	
+	public post(int id, int count, String tId){
+		termIdCount = count;
+		docId = id;
+		termId = tId;		
+	}
+	
+	public int getTermCount(){
+		return termIdCount;
+	}
+	
+	public int getDocId(){
+		return docId;
+	}
+	
+	public String getTermId(){
+		return termId;
+	}
+	
+	
+	
+	
+
+}
