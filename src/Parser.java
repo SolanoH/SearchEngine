f31829700e959d0b3c3601df310eebeae37ec2aa
Hector Solano <solanoh@me.com>
2017-03-01 18:42:49 -0800

    Implemented Word Frequency

--- src/Parser.java
+++ src/Parser.java
@@ -1,79 +1,125 @@
-import java.io.File;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 
-/**
- * 
- */
-
-/**
- * @author Hector
-
- *
- */
-
-
 /* here is the skeleton for the parser, you can use Jsoup to help you parse the html files,
- * feel free to add any member variables and methods you may beed.  You may also modify
+ * feel free to add any member variables and methods you may need.  You may also modify
  * the skeletons of the methods I added but they have to return the concurrent maps.
  */
 
-public class Parser {
+public class Parser
+{
 	int docId;
-	File htmlFile;
-	ConcurrentHashMap<String, Integer> wordFreq;
+	Document htmlDocument;
+	ConcurrentHashMap< String, Integer > wordFreq;
+	//final private ConcurrentHashMap< String, List< String > > documentInformation = new ConcurrentHashMap<>();
+	private ConcurrentHashMap< String, List< String > > documentInformation;
 
 
 	@SuppressWarnings("rawtypes")
-	public Parser(File file, int id) {
+	public Parser( Document document, int id )
+	{
 		docId = id;
-		htmlFile = file;
-			
-	}
-	
-	// Returns a metadata of html file
-	public ConcurrentHashMap<String, String>  getMetaData() {
-		// TODO Auto-generated constructor stub
-		/*
-		if meta data exist add the meta data else return empty map.
-		ConcurrentHashMap<String, String> metaData; = new ConcurrentHashMap<String, String>();;
-		return metaData
-		*/
-	}
-	
-	// Returns authors of htlm file
-	public ConcurrentHashMap<String, String> getAuthors() {
-		// TODO Auto-generated constructor stub
-		/*
-		ConcurrentHashMap<String, String> zones = new ConcurrentHashMap<String, String>();
-		if authors exist put them in map else return empty map
-		authors.put("author", "Hector Solano");
-		authors.put("author", "Mario  Sanches"); 
-		return zones
-		*/
+		htmlDocument = document;
+		wordFreq = new ConcurrentHashMap<>();
+		Table< String ,Integer,LinkedList<Integer>> twoDimensionalFileMap = HashBasedTable.create();
 
+
+		//initializeConcurrentHashMap();
+	}
+
+	/**
+	 * Returns a metadata of html file
+	 * @return ConcurrentHashMap containing all metadata
+     */
+	public ConcurrentHashMap< String, List< String > > initializeConcurrentHashMap()
+	{
+//		synchronized ( documentInformation )
+//		{
+//			for( Element tag : htmlDocument.getElementsByTag( "meta" ) )
+//			{
+//				if( !documentInformation.containsKey( tag.attr("name") ) )
+//					documentInformation.put( tag.attr( "name" ), Collections.synchronizedList( new ArrayList<>() ) );
+//				documentInformation.get( tag.attr("name") ).add( tag.attr("content") );
+//			}
+//		}
+
+		documentInformation = new ConcurrentHashMap<>();
+		for( Element tag : htmlDocument.getElementsByTag( "meta" ) )
+		{
+			if( !documentInformation.containsKey( tag.attr("name") ) )
+				documentInformation.put( tag.attr( "name" ), Collections.synchronizedList( new ArrayList<>() ) );
+			documentInformation.get( tag.attr("name") ).add( tag.attr("content") );
+		}
+
+		return documentInformation;
+	}
+
+	/**
+	 * Returns authors of htlm file
+	 */
+	public List< String > getAuthors()
+	{
+//		ConcurrentHashMap< String, String > authors = new ConcurrentHashMap<>();
+//		for( Element tag : htmlDocument.getElementsByTag( "meta" ) )
+//				if( tag.attr( "name" ).equals( "author" ) )
+//					authors.put( tag.attr( "content" ), tag.attr( "content" ) );
+		return documentInformation.get( "authors" );
 	}
 	
 	// Returns Zones of html files.
-	public ConcurrentHashMap<String, String> getZones() {
-		// TODO Auto-generated constructor stub
-		/*
-		ConcurrentHashMap<String, String> zones = new ConcurrentHashMap<String, String>();
-		if zones exist add them else return empty map (right now we are only gathering 
-			zones.put("title", "Hello Word");
-			zones.put("abstract", "None")
-			zones.put("h1" "h1 data")
-			zones.put("h2" "h2 data")
-			zones.put("h3" "h3 data")
-			zones.put("h(n)" "h(n) data )
-			etc.
-		return zones
-		*/
-	
-	public wordFrequency(){
-		// TODO Auto-generated constructor stub
+	public ConcurrentHashMap<String, List< String > > getZones()
+	{
+
+		ConcurrentHashMap< String, List< String > > zones = new ConcurrentHashMap<>();
+//		zones.put( "title", htmlDocument.title() );
+//
+//		for( int value = 1; value < 7; value++ )
+//			for( Element tag : htmlDocument.select( "h" + value ) )
+//				zones.put( "h"+value, tag.ownText() );
+//
+//		for( Map.Entry< String, String > v : zones.entrySet() )
+//		 	System.out.println( v.toString() );
+
+
+		return zones;
+
+
+
+		 /*
+		 ConcurrentHashMap<String, String> zones = new ConcurrentHashMap<String, String>();
+		 if zones exist add them else return empty map (right now we are only gathering
+		 	zones.put("title", "Hello Word");
+		 	zones.put("abstract", "None")
+		 	zones.put("h1" "h1 data")
+		 	zones.put("h2" "h2 data")
+		 	zones.put("h3" "h3 data")
+
+
+		 	h3 -> sunny
+
+
+		 	zones.put("h(n)" "h(n) data )
+		 	etc.
+		 return zones
+		 */
 	}
-	
-	
+
+	public void wordFrequency()
+	{
+		for( String line : htmlDocument.html().split( "\n" ) )
+			for( String word : Jsoup.parse( line ).text().replaceAll( "[^a-zA-Z0-9]+", " " ).split( "\\s+" ) )
+				if( !word.isEmpty() )
+					wordFreq.put( word, ( wordFreq.containsKey( word ) ? wordFreq.get( word ) + 1 : 1 ) );
+	}
 
 }
